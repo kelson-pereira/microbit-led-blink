46a08807159d2970280c0623e4aff3f257ea858a
Kelson Pereira <kelson.op@gmail.com>
2021-04-04 12:55:45 -0300

    initial commit

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+function ready () {
+    blink = 0
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.playTone(1976, music.beat(BeatFraction.Sixteenth))
+    basic.clearScreen()
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (blink == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        blink = 1
+    } else {
+        ready()
+    }
+})
+let blink = 0
+ready()
 basic.forever(function () {
-	
+    if (blink == 1) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        music.playTone(988, music.beat(BeatFraction.Sixteenth))
+        basic.showIcon(IconNames.Heart)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.clearScreen()
+        basic.pause(200)
+    }
 })
